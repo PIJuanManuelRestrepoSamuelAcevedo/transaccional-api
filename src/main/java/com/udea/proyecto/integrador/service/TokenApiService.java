d54bd4dbd210191e46006defc8e8ae5a53430b1f
Juan Manuel Restrepo <juanrestrepo1221@gmail.com>
2024-10-17 22:06:49 -0500

    fix beans errors

--- src/main/java/com/udea/proyecto/integrador/service/TokenApiService.java
+++ src/main/java/com/udea/proyecto/integrador/service/TokenApiService.java
@@ -1,9 +1,12 @@
 package com.udea.proyecto.integrador.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@Service
 public class TokenApiService {
+
     @Autowired
     private WebClient.Builder webClientBuilder;
 
